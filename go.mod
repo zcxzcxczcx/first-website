fa4659f7553ebe2b3499ed3e751f73ca3c3db8e4
wxzhengchengxia <1637960435@qq.com>
2020-11-23 18:01:33 +0800

    开始研究go-grpc

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module first-website
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-contrib/sessions v0.0.3
+	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/protobuf v1.4.1
+	google.golang.org/grpc v1.33.2
+)
--- main.go
+++ main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("dddddddddddddddddddddddddddddd")
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+	r.GET("/hello", func(c *gin.Context) {
+		session := sessions.Default(c)
+		if session.Get("hello") != "world" {
+			session.Set("hello", "world")
+			session.Save()
+		}
+		c.JSON(200, gin.H{"hello": session.Get("hello")})
 	})
-	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.RunTLS(":8000", "/Users/abc/127.0.0.1.pem",
+		"/Users/abc/127.0.0.1-key.pem")
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	var result []int
+	var inorder func(root *TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		inorder(root.Left)
+		result = append(result, root.Val)
+		inorder(root.Right)
+	}
+
+	return result
+
 }
